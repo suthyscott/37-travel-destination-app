fa9d1aba9ea2431247b7b9833faab741a7bc3283
suthyscott <sutherlandscott21@gmail.com>
2023-05-01 10:01:57 -0600

    setting up files and api key

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/Header"
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      App.jsx
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            Header.jsx
+        </div>
+    )
+}
+
+export default Header
