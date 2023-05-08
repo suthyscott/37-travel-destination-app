36b96bceb38a397c15ea9538290dc157c5e0c1bd
suthyscott <sutherlandscott21@gmail.com>
2023-05-08 10:01:31 -0600

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,19 @@ import './App.css';
 import Header from "./components/Header/Header"
 import Home from './components/Home'
 import HooksExampleOne from './components/HooksExampleOne';
+import {Routes, Route} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Home/>
-      {/* <HooksExampleOne /> */}
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/hooks' element={<HooksExampleOne/>}/>
+      </Routes>
+
+      
     </div>
   );
 }
