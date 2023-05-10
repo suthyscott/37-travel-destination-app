824ace699aa3b870dd228fbcd02fe49342cc4cac
suthyscott <sutherlandscott21@gmail.com>
2023-05-10 09:57:10 -0600

    building add form

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 
 const AddDestination = () => {
+  const [name, setName] = useState('')
+  const [imageURL, setImageURL] = useState('')
+  const [international, setInternational] = useState(false)
+  const [notes, setNotes] = useState('')
+
+  console.log(international)
   return (
-    <div>AddDestination</div>
+    <form>
+      <input placeholder='Destination name' onChange={e => setName(e.target.value)} value={name}/>
+      <input placeholder='Image url' onChange={e => setImageURL(e.target.value)} value={imageURL}/>
+      <input type='checkbox' checked={international ? true : false} onChange={e => setInternational(!international)} />
+      <input placeholder='Enter notes here' onChange={e => setNotes(e.target.value)} value={notes}/>
+
+    </form>
   )
 }
 
