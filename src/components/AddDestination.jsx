7e05546b0a13ba575fab1e6171eccb7a55bbee74
suthyscott <sutherlandscott21@gmail.com>
2023-05-10 16:29:37 -0600

    finished post request

--- src/components/AddDestination.jsx
+++ src/components/AddDestination.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import axios from 'axios'
 
 const AddDestination = () => {
   const [name, setName] = useState('')
@@ -7,13 +8,31 @@ const AddDestination = () => {
   const [notes, setNotes] = useState('')
 
   console.log(international)
+
+  const submitHandler = e => {
+    e.preventDefault()
+
+    let bodyInfo = {
+      name,
+      imageURL,
+      international,
+      notes
+    }
+
+    axios.post(`http://localhost:4545/api/destinations?apiKey=${process.env.REACT_APP_TRAVEL_DESTINATION_KEY}`, bodyInfo)
+      .then(res => {
+        console.log(res)
+      })
+      .catch(err => console.log(err))
+
+  }
   return (
-    <form>
+    <form onSubmit={e => submitHandler(e)}>
       <input placeholder='Destination name' onChange={e => setName(e.target.value)} value={name}/>
       <input placeholder='Image url' onChange={e => setImageURL(e.target.value)} value={imageURL}/>
       <input type='checkbox' checked={international ? true : false} onChange={e => setInternational(!international)} />
       <input placeholder='Enter notes here' onChange={e => setNotes(e.target.value)} value={notes}/>
-
+      <button>Submit</button>
     </form>
   )
 }
